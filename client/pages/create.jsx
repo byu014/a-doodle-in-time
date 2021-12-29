ee2e3f31d29ac435d51107746ada03822fb0fb06
Bailey Yu <bailey.yu@yahoo.com>
2021-12-28 21:06:34 -0800

    modified minor css

--- client/pages/create.jsx
+++ client/pages/create.jsx
@@ -11,7 +11,7 @@ export default function Create(props) {
       </div>
       <form className="col-30 submission-form">
           <input className="submit-title large-font" type="text" name="title" id="title" placeholder="Add a title!"/>
-          <textarea rows='3' className="submit-caption" type="text" name="caption" id="caption" placeholder="Write a caption!" autoComplete='off'/>
+          <textarea rows='10' className="submit-caption" type="text" name="caption" id="caption" placeholder="Write a caption!" autoComplete='off'/>
           <div className="button-flex">
             <button className="submit-button blue-btn" type="submit">Submit!</button>
           </div>
